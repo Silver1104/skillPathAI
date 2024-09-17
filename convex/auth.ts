f586dec682898c5e7cdae1256b5757fb8407328e
Silver1104 <raghavpoddar39@gmail.com>
2024-09-17 14:58:55 +0530

    fifth commit

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,7 @@
 import { convexAuth } from "@convex-dev/auth/server";
 import GitHub from '@auth/core/providers/github';
 import Google from '@auth/core/providers/google';
+import Resend from '@auth/core/providers/resend';
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [GitHub, Google],
+  providers: [GitHub, Google, Resend],
 });
